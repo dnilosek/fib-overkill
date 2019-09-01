970ba5a062d274b4d255b80ea5a7442cfc71fc26
dnilosek <david.nilosek@gmail.com>
2019-09-01 13:23:56 -0700

    Added k8s deploy

--- build/travis/deploy.sh
+++ build/travis/deploy.sh
@@ -2,11 +2,21 @@
 
 # Build binaries and images (dep needs to be run already)
 make build
-docker build -t dnilosek/fib-overkill-api -f api/dockerfile .
-docker build -t dnilosek/fib-overkill-worker -f worker/dockerfile .
-docker build -t dnilosek/fib-overkill-web ./web
+docker build -t dnilosek/fib-overkill-api:latest -t dnilosek/fib-overkill-api:$SHA -f api/dockerfile .
+docker build -t dnilosek/fib-overkill-worker:latest -t dnilosek/fib-overkill-worker:$SHA -f worker/dockerfile .
+docker build -t dnilosek/fib-overkill-web:latest -t dnilosek/fib-overkill-web:$SHA ./web
 
 # Push to dockerhub
-docker push dnilosek/fib-overkill-api
-docker push dnilosek/fib-overkill-worker
-docker push dnilosek/fib-overkill-web
+docker push dnilosek/fib-overkill-api:latest
+docker push dnilosek/fib-overkill-api:$SHA
+
+docker push dnilosek/fib-overkill-worker:latest
+docker push dnilosek/fib-overkill-worker:$SHA
+
+docker push dnilosek/fib-overkill-web:latest
+docker push dnilosek/fib-overkill-web:$SHA
+
+# Apply k8s configs
+kubectl version
+kubectl apply -f build/k8s
+kubectl rollout restart
